aae083e8a1c52feb7f36512169a65132c266fb15
root27 <oguzhaneee@gmail.com>
2022-04-20 14:51:29 +0300

    Login page started

--- App.js
+++ App.js
@@ -5,10 +5,7 @@ import LoginScreen from "./screens/LoginScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LoginScreen />
   );
 }
 
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,58 @@
-import { View, Text } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Pressable, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-const LoginScreen = () => {
+export default function LoginScreen() {
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={styles.container}>
+      <TextInput style={styles.mail} placeholder="Email" />
+      <TextInput style={styles.password} placeholder="Password" secureTextEntry />
+        <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Register</Text>
+        </TouchableOpacity>
     </View>
   )
 }
 
-export default LoginScreen
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    mail: {
+        borderRadius: 15,
+        width: '70%',
+        borderColor: '#000',
+        borderWidth: 1,
+        marginBottom: 10,
+        padding: 10,
+    },
+    password: {
+        borderRadius: 15,
+        width: '70%',
+        borderColor: '#000',
+        borderWidth: 1,
+        padding: 10,
+    },
+    button: {
+        
+        width: '40%',
+        backgroundColor: '#2f95dc',
+        padding: 10,
+        marginTop: 20,
+        borderRadius: 15,
+    },
+    buttonText: {
+        color: '#fff',
+        textAlign: 'center',
+    },
+    signup: {
+        marginTop: 20,
+        color: '#1e90ff',
+        textAlign: 'center',
+    }
+})
