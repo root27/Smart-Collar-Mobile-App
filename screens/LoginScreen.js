90429d7709cc7498b1d302ab58a9ed7a5b43448d
root27 <oguzhaneee@gmail.com>
2022-04-21 09:32:43 +0300

    Login page customized

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,58 +1,42 @@
-import { StyleSheet, Text, View, TextInput, Pressable, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native'
 import React from 'react'
+import { Button, Input } from "@rneui/themed"
+
 
 export default function LoginScreen() {
+
+    const [email, setEmail] = React.useState("")
+    const [password, setPassword] = React.useState("")
+
+
   return (
-    <View style={styles.container}>
-      <TextInput style={styles.mail} placeholder="Email" />
-      <TextInput style={styles.password} placeholder="Password" secureTextEntry />
-        <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>Register</Text>
-        </TouchableOpacity>
-    </View>
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <View style={styles.inputContainer}>
+        <Input placeholder="Email" autoFocus type="Email" value={email} onChangeText={(text) => setEmail(text)} />
+        <Input placeholder="Password" secureTextEntry type="Password" value={password} onChangeText={(text) => setPassword(text)} />
+      </View>
+
+      <Button containerStyle={styles.button} title={"Login"}  />
+      <Button containerStyle={styles.button} type="outline"  title={"Register"} />
+    </KeyboardAvoidingView>
   )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+      flex : 1,
+      alignItems: 'center',
+      backgroundColor: "white",
+      padding : 10,
     },
-    mail: {
-        borderRadius: 15,
-        width: '70%',
-        borderColor: '#000',
-        borderWidth: 1,
-        marginBottom: 10,
-        padding: 10,
+    inputContainer: {
+        width: 300
     },
-    password: {
-        borderRadius: 15,
-        width: '70%',
-        borderColor: '#000',
-        borderWidth: 1,
-        padding: 10,
-    },
-    button: {
-        
-        width: '40%',
-        backgroundColor: '#2f95dc',
-        padding: 10,
+    button:{
+        width: 200,
         marginTop: 20,
-        borderRadius: 15,
-    },
-    buttonText: {
-        color: '#fff',
-        textAlign: 'center',
-    },
-    signup: {
-        marginTop: 20,
-        color: '#1e90ff',
-        textAlign: 'center',
+
     }
+
+ 
 })
